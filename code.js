826cc974953762403de3414dc68398185d1dc7de
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-09-15 15:48:12 -0500

    added question 3

--- code.js
+++ code.js
@@ -4,8 +4,9 @@ function helloWorld() {
 }
 
 function sayHello() {
-    return "Hello, World!";
+    return "Hello, Jane!";
 }
+
 
 
 
--- tests.js
+++ tests.js
@@ -18,8 +18,17 @@ describe('helloWorld', function() {
 
 describe('sayHello', function() {
     it('should be a defined function', function () {
-        expect(typeof helloWorld).toBe('function');
-    })
-})
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should be a defined function', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return the string "Hello, Jane" when executed', function() {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
+});
+
+
+
 
 
